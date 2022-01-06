0a3eb80f12eb2ce15d1c2a92be187b1361eb8ef6
AngelDevelop <angelpublicista.1@gmail.com>
2022-01-06 16:21:51 -0500

    final componente pages

---
+++ components/Cardmenu.jsx
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.css";
+import styles from "../styles/Cardmenu.module.css";
+import Image from "next/image";
+
+export default function Cardmenu({fondo, title, subtitle, link, white}) {
+
+  let modeClass = ""
+  if(white){
+    modeClass = styles.rwCardmenuWhite
+  }
+
+  return (
+    <div className={`${modeClass} ${styles.rwCardmenu}`}>
+      <Image 
+        className={styles.rwCardmenuFondo}
+        src={fondo}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className={styles.rwCardmenuContent}>
+        <h2>{title}</h2>
+        <span>{subtitle}</span>
+        <a href={link}>Ver Más</a>
+      </div>
+    </div>
+  );
+}
--- pages/components.jsx
+++ pages/components.jsx
@@ -1,6 +1,8 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Chapters from "../components/Chapters"
+import Cardmenu from "../components/Cardmenu"
+import FondoPrueba from "../public/img/fondo-cardmenu.png"
 
 
 
@@ -8,7 +10,11 @@ export default function components() {
 
     return (
         <div>
-            <Header />
+            <Header
+            />
+            <Header
+                dark={true}
+            />
 
             <div className="container mt-5 mb-5">
                 <div className="row">
@@ -35,6 +41,20 @@ export default function components() {
                     </div>
                 </div>
             </div>
+
+            <Cardmenu
+                fondo= {FondoPrueba}
+                title= 'ROBERT WEST'
+                subtitle= 'y la escuela de Berkeley'
+                link= "#"
+            />
+            <Cardmenu
+                fondo= {FondoPrueba}
+                title= 'ROBERT WEST'
+                subtitle= 'y la escuela de Berkeley'
+                link= "#"
+                white={true}
+            />
         <Footer />
         </div>
     )
