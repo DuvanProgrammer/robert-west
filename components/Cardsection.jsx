5945f9ed0746b1b3f368638c9312ff2919ede7c1
AngelDevelop <angelpublicista.1@gmail.com>
2022-01-06 19:48:13 -0500

    final card section

---
+++ components/Cardsection.jsx
@@ -0,0 +1,31 @@
+import "bootstrap/dist/css/bootstrap.css"
+import styles from "../styles/Cardsection.module.css"
+import Image from "next/image"
+import Link from "next/Link"
+
+export default function Cardsection({imagen, title, link}) {
+    return (
+        <div className={styles.rwCardsectionItem}>
+            <Image
+            className={styles.rwCardsectionImg}
+            src={imagen}
+            objectFit="cover"
+            layout="fill"
+            />
+            <div className={styles.rwCardsectionTitle}>
+                <h2>{title}</h2>
+                    <Link href={link}>
+                        <a>
+                            <Image
+                                className={styles.rwCardsectionView}
+                                src="/img/arrow-view.png"
+                                width={30}
+                                height={30}
+                                objectFit="cover"
+                            />
+                        </a>
+                    </Link>
+            </div>
+        </div>
+    )
+}
--- pages/components.jsx
+++ pages/components.jsx
@@ -1,62 +1,90 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import Chapters from "../components/Chapters"
-import Cardmenu from "../components/Cardmenu"
-import FondoPrueba from "../public/img/fondo-cardmenu.png"
-import SubheaderBlack from "../components/SubheaderBlack"
-
-
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Chapters from "../components/Chapters";
+import Cardmenu from "../components/Cardmenu";
+import FondoPrueba from "../public/img/fondo-cardmenu.png";
+import SubheaderBlack from "../components/SubheaderBlack";
+import Cardsection from "../components/Cardsection";
+import ImagenPrueba from "../public/img/card-img.png";
 
 export default function components() {
-
-    return (
-        <div>
-            <Header
+  return (
+    <div>
+      <Header />
+      <Header dark={true} />
+      <SubheaderBlack></SubheaderBlack>
+      <div className="container mt-5 mb-5">
+        <div className="row">
+          <div className="col-12 col-md-4">
+            <Chapters
+              chapter="Capítulo 1"
+              open="https://www.youtube.com/"
+              download="https://www.facebook.com/"
             />
-            <Header
-                dark={true}
+          </div>
+          <div className="col-12 col-md-4">
+            <Chapters
+              chapter="Capítulo 2"
+              open="https://www.instagram.com/?hl=es-la"
+              download="https://twitter.com/?lang=es"
             />
-            <SubheaderBlack></SubheaderBlack>
-            <div className="container mt-5 mb-5">
-                <div className="row">
-                    <div className="col-12 col-md-4">
-                        <Chapters 
-                            chapter= 'Capítulo 1'
-                            open= "https://www.youtube.com/"
-                            download= "https://www.facebook.com/"
-                        />
-                    </div>
-                    <div className="col-12 col-md-4">
-                        <Chapters 
-                            chapter= 'Capítulo 2'
-                            open= "https://www.instagram.com/?hl=es-la"
-                            download= "https://twitter.com/?lang=es"
-                        />
-                    </div>
-                    <div className="col-12 col-md-4">
-                        <Chapters 
-                            chapter= 'Capítulo 3'
-                            open= "https://co.linkedin.com/"
-                            download= "https://vimeo.com/es"
-                        />
-                    </div>
-                </div>
-            </div>
-
-            <Cardmenu
-                fondo= {FondoPrueba}
-                title= 'ROBERT WEST'
-                subtitle= 'y la escuela de Berkeley'
-                link= "#"
+          </div>
+          <div className="col-12 col-md-4">
+            <Chapters
+              chapter="Capítulo 3"
+              open="https://co.linkedin.com/"
+              download="https://vimeo.com/es"
             />
-            <Cardmenu
-                fondo= {FondoPrueba}
-                title= 'ROBERT WEST'
-                subtitle= 'y la escuela de Berkeley'
-                link= "#"
-                white={true}
-            />
-        <Footer />
+          </div>
         </div>
-    )
+      </div>
+
+      <Cardmenu
+        fondo={FondoPrueba}
+        title="ROBERT WEST"
+        subtitle="y la escuela de Berkeley"
+        link="#"
+      />
+      <Cardmenu
+        fondo={FondoPrueba}
+        title="ROBERT WEST"
+        subtitle="y la escuela de Berkeley"
+        link="#"
+        white={true}
+      />
+      <div className="container mt-5 mb-5">
+        <div className="row">
+          <div className="col-12 col-md-3">
+          <Cardsection 
+            imagen={ImagenPrueba} 
+            title="CATEGORIAS"
+            link="#"
+         />
+          </div>
+          <div className="col-12 col-md-3">
+          <Cardsection 
+            imagen={ImagenPrueba} 
+            title="CATEGORIAS"
+            link="#"
+          />
+          </div>
+          <div className="col-12 col-md-3">
+          <Cardsection 
+            imagen={ImagenPrueba} 
+            title="CATEGORIAS"
+            link="#"
+          />
+          </div>
+          <div className="col-12 col-md-3">
+          <Cardsection 
+            imagen={ImagenPrueba} 
+            title="CATEGORIAS"
+            link="#"
+          />
+          </div>
+        </div>
+      </div>  
+      <Footer />
+    </div>
+  );
 }
