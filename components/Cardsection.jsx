69764a32476ca6aeec4a0f7302de9254e16b9e43
AngelDevelop <angelpublicista.1@gmail.com>
2022-01-21 16:25:59 -0500

    Corecciones diego

--- components/Cardsection.jsx
+++ components/Cardsection.jsx
@@ -1,30 +1,36 @@
-import styles from "../styles/Cardsection.module.css"
-import Image from "next/image"
-import Link from "next/link"
+import styles from "../styles/Cardsection.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Cardsection({imagen, title, link}) {
-    return (
-        <div className={styles.rwCardsectionItem}>
+export default function Cardsection({ imagen, title, link }) {
+  return (
+    <div className={styles.rwContCardsectionItem}>
+      <Link href={link}>
+        <a>
+          <div className={styles.rwCardsectionItem}>
             <Image
-            className={styles.rwCardsectionImg}
-            src={imagen}
-            objectFit="cover"
-            layout="fill"
+              className={styles.rwCardsectionImg}
+              src={imagen}
+              objectFit="cover"
+              layout="fill"
             />
             <div className={styles.rwCardsectionTitle}>
-                <h2>{title}</h2>
-                    <Link href={link}>
-                        <a>
-                            <Image
-                                className={styles.rwCardsectionView}
-                                src="/img/arrow-view.png"
-                                width={30}
-                                height={30}
-                                objectFit="cover"
-                            />
-                        </a>
-                    </Link>
+              <h2>{title}</h2>
+              <Link href="#">
+                <a>
+                  <Image
+                    className={styles.rwCardsectionView}
+                    src="/img/arrow-view.png"
+                    width={30}
+                    height={30}
+                    objectFit="cover"
+                  />
+                </a>
+              </Link>
             </div>
-        </div>
-    )
+          </div>
+        </a>
+      </Link>
+    </div>
+  );
 }
