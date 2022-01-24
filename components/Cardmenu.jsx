14f80bc59d715541db48fd022dd7dea29c0af951
AngelDevelop <angelpublicista.1@gmail.com>
2022-01-24 15:48:30 -0500

    Avance ajustes

--- components/Cardmenu.jsx
+++ components/Cardmenu.jsx
@@ -1,7 +1,17 @@
 import styles from "../styles/Cardmenu.module.css";
 import Image from "next/image";
+import { useEffect } from 'react'
+import AOS from 'aos'
+
+import 'aos/dist/aos.css'
 
 export default function Cardmenu({fondo, title, subtitle, link, white}) {
+  useEffect(() => {
+    // here you can add your aos options
+    AOS.init({
+      offset: 100,
+    });
+  }, []);
 
   let modeClass = ""
   if(white){
@@ -16,9 +26,10 @@ export default function Cardmenu({fondo, title, subtitle, link, white}) {
         layout="fill"
         objectFit="cover"
       />
-      <div className={styles.rwCardmenuContent}
-           data-aos="fade-left"
-           data-aos-duration="1200">
+      <div 
+        data-aos={white ? "fade-left" : "fade-right"}
+        data-aos-duration="1500"
+        className={styles.rwCardmenuContent}>
         <h2>{title}</h2>
         <span>{subtitle}</span>
         <a href={link}>Ver Más</a>
